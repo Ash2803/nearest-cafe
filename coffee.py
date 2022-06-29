0d7bc6e088b3bc0f2f900c1ffe0dcddb983e0a1c
Ashot Abazyan <ashot.93@mail.ru>
2022-06-30 02:17:00 +0300

    29/06

--- coffee.py
+++ coffee.py
@@ -5,13 +5,14 @@ with open("coffee.json", "r", encoding='CP1251') as my_file:
     file_contents = my_file.read()
 
 cafe = json.loads(file_contents)
-# print(cafe)
+print(cafe)
 
+for name in cafe:
+    print(name.get('Name'), name.get('geoData: coordinates'))
 # for country in capitals:
 #   capital = capitals[country]
 #   print("столица {} это {}".format(country,capital))
 # first_cafe_name = cafe[0]['Name']
 # first_cafe_cord = cafe[0]['geoData']['coordinates']
-# print(first_cafe_name, *first_cafe_cord)
-
+# print(first_cafe_name, *first_cafe_cord
 
