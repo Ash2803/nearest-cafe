0862c593381a90a9418da21aaa4d1491615545f1
Ashot Abazyan <ashot.93@mail.ru>
2022-07-03 23:16:19 +0300

    03/07

--- coffee.py
+++ coffee.py
@@ -5,11 +5,6 @@ from geopy import distance
 import requests
 from dotenv import load_dotenv
 
-with open("coffee.json", "r", encoding='CP1251') as my_file:
-    file_contents = my_file.read()
-
-cafe_data = json.loads(file_contents)
-
 
 def fetch_coordinates(apikey, address):
     base_url = "https://geocode-maps.yandex.ru/1.x"
@@ -29,21 +24,25 @@ def fetch_coordinates(apikey, address):
     return lat, lon
 
 
-load_dotenv()
-apikey = os.getenv('YANDEX_GEO_API_KEY')
-your_place = input("Где вы находитесь? ")
-your_place_coords = fetch_coordinates(apikey, your_place)
-
-for cafes in cafe_data:
-    cafe_dict = dict()
-    cafe_coords = cafes.get('Latitude_WGS84'), cafes.get('Longitude_WGS84')
-    distance_to_cafe = distance.distance(your_place_coords, cafe_coords).km
-    cafe_dict['title'] = cafes.get('Name')
-    cafe_dict['dist'] = distance_to_cafe
-    cafe_dict['latitude'] = cafes.get('Latitude_WGS84')
-    cafe_dict['longitude'] = cafes.get('Longitude_WGS84')
-    cafe_list.append(cafe_dict)
+def list_of_cafes(cafe_list, your_place_coords):
+    for cafes in cafe_data:
+        cafe_dict = dict()
+        cafe_coords = cafes.get('Latitude_WGS84'), cafes.get('Longitude_WGS84')
+        distance_to_cafe = distance.distance(your_place_coords, cafe_coords).km
+        cafe_dict['title'] = cafes.get('Name')
+        cafe_dict['dist'] = distance_to_cafe
+        cafe_dict['latitude'] = cafes.get('Latitude_WGS84')
+        cafe_dict['longitude'] = cafes.get('Longitude_WGS84')
+        cafe_list.append(cafe_dict)
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    with open("coffee.json", "r", encoding='CP1251') as my_file:
+        file_contents = my_file.read()
+    cafe_data = json.loads(file_contents)
+    apikey = os.getenv('YANDEX_GEO_API_KEY')
+    your_place = input("Где вы находитесь? ")
+    your_place_coords = fetch_coordinates(apikey, your_place)
     cafe_list = []
+    pprint(cafe_list, sort_dicts=False)
